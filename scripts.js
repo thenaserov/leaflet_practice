927bebf268200945417decbb1c3dba4b66db80f9
nasrez <naser.rezayi.nr@gmail.com>
2023-12-08 22:50:53 -0800

    [REF] cleaned for WMS/WFS work

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([33.0639, 46.9092], 9);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -7,183 +7,183 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 // tutorial 3
-var LeafIcon = L.Icon.extend({
-    options: {
-        shadowUrl: 'leaf-shadow.png',
-        iconSize:     [38, 95],
-        shadowSize:   [50, 64],
-        iconAnchor:   [22, 94],
-        shadowAnchor: [4, 62],
-        popupAnchor:  [-3, -76]
-    }
-});
+// var LeafIcon = L.Icon.extend({
+//     options: {
+//         shadowUrl: 'leaf-shadow.png',
+//         iconSize:     [38, 95],
+//         shadowSize:   [50, 64],
+//         iconAnchor:   [22, 94],
+//         shadowAnchor: [4, 62],
+//         popupAnchor:  [-3, -76]
+//     }
+// });
 
 
-var greenIcon = new LeafIcon({iconUrl: 'leaf-green.png'}),
-    redIcon = new LeafIcon({iconUrl: 'leaf-red.png'}),
-    orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
+// var greenIcon = new LeafIcon({iconUrl: 'leaf-green.png'}),
+//     redIcon = new LeafIcon({iconUrl: 'leaf-red.png'}),
+//     orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
 
 
-L.icon = function (options) {
-    return new L.Icon(options);
-};
+// L.icon = function (options) {
+//     return new L.Icon(options);
+// };
 
 
-L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
-L.marker([51.495, -0.083], {icon: redIcon}).addTo(map).bindPopup("I am a red leaf.");
-L.marker([51.49, -0.1], {icon: orangeIcon}).addTo(map).bindPopup("I am an orange leaf.");
+// L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+// L.marker([51.495, -0.083], {icon: redIcon}).addTo(map).bindPopup("I am a red leaf.");
+// L.marker([51.49, -0.1], {icon: orangeIcon}).addTo(map).bindPopup("I am an orange leaf.");
 
-// tutorial 4
-var marker = L.marker([50.4501, 30.5234],
-    {alt: 'Kyiv'}).addTo(map) // "Kyiv" is the accessible name of this marker
-    .bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+// // tutorial 4
+// var marker = L.marker([50.4501, 30.5234],
+//     {alt: 'Kyiv'}).addTo(map) // "Kyiv" is the accessible name of this marker
+//     .bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
 
 
-// tutorial 5
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
+// // tutorial 5
+// var geojsonFeature = {
+//     "type": "Feature",
+//     "properties": {
+//         "name": "Coors Field",
+//         "amenity": "Baseball Stadium",
+//         "popupContent": "This is where the Rockies play!"
+//     },
+//     "geometry": {
+//         "type": "Point",
+//         "coordinates": [-104.99404, 39.75621]
+//     }
+// };
 
-L.geoJSON(geojsonFeature).addTo(map);
+// L.geoJSON(geojsonFeature).addTo(map);
 
-var myLines = [{
-    "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-    "type": "LineString",
-    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
+// var myLines = [{
+//     "type": "LineString",
+//     "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+// }, {
+//     "type": "LineString",
+//     "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+// }];
 
-var myLayer = L.geoJSON().addTo(map);
-myLayer.addData(geojsonFeature);
+// var myLayer = L.geoJSON().addTo(map);
+// myLayer.addData(geojsonFeature);
 
-var myLines = [{
-    "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-    "type": "LineString",
-    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
+// var myLines = [{
+//     "type": "LineString",
+//     "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+// }, {
+//     "type": "LineString",
+//     "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+// }];
 
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
+// var myStyle = {
+//     "color": "#ff7800",
+//     "weight": 5,
+//     "opacity": 0.65
+// };
 
-L.geoJSON(myLines, {
-    style: myStyle
-}).addTo(map);
+// L.geoJSON(myLines, {
+//     style: myStyle
+// }).addTo(map);
 
-var states = [{
-    "type": "Feature",
-    "properties": {"party": "Republican"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-104.05, 48.99],
-            [-97.22,  48.98],
-            [-96.58,  45.94],
-            [-104.03, 45.94],
-            [-104.05, 48.99]
-        ]]
-    }
-}, {
-    "type": "Feature",
-    "properties": {"party": "Democrat"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-109.05, 41.00],
-            [-102.06, 40.99],
-            [-102.03, 36.99],
-            [-109.04, 36.99],
-            [-109.05, 41.00]
-        ]]
-    }
-}];
+// var states = [{
+//     "type": "Feature",
+//     "properties": {"party": "Republican"},
+//     "geometry": {
+//         "type": "Polygon",
+//         "coordinates": [[
+//             [-104.05, 48.99],
+//             [-97.22,  48.98],
+//             [-96.58,  45.94],
+//             [-104.03, 45.94],
+//             [-104.05, 48.99]
+//         ]]
+//     }
+// }, {
+//     "type": "Feature",
+//     "properties": {"party": "Democrat"},
+//     "geometry": {
+//         "type": "Polygon",
+//         "coordinates": [[
+//             [-109.05, 41.00],
+//             [-102.06, 40.99],
+//             [-102.03, 36.99],
+//             [-109.04, 36.99],
+//             [-109.05, 41.00]
+//         ]]
+//     }
+// }];
 
-L.geoJSON(states, {
-    style: function(feature) {
-        switch (feature.properties.party) {
-            case 'Republican': return {color: "#ff0000"};
-            case 'Democrat':   return {color: "#0000ff"};
-        }
-    }
-}).addTo(map);
+// L.geoJSON(states, {
+//     style: function(feature) {
+//         switch (feature.properties.party) {
+//             case 'Republican': return {color: "#ff0000"};
+//             case 'Democrat':   return {color: "#0000ff"};
+//         }
+//     }
+// }).addTo(map);
 
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
+// var geojsonMarkerOptions = {
+//     radius: 8,
+//     fillColor: "#ff7800",
+//     color: "#000",
+//     weight: 1,
+//     opacity: 1,
+//     fillOpacity: 0.8
+// };
 
-L.geoJSON(someGeojsonFeature, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
-    }
-}).addTo(map);
+// L.geoJSON(someGeojsonFeature, {
+//     pointToLayer: function (feature, latlng) {
+//         return L.circleMarker(latlng, geojsonMarkerOptions);
+//     }
+// }).addTo(map);
 
-function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
-}
+// function onEachFeature(feature, layer) {
+//     // does this feature have a property named popupContent?
+//     if (feature.properties && feature.properties.popupContent) {
+//         layer.bindPopup(feature.properties.popupContent);
+//     }
+// }
 
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
+// var geojsonFeature = {
+//     "type": "Feature",
+//     "properties": {
+//         "name": "Coors Field",
+//         "amenity": "Baseball Stadium",
+//         "popupContent": "This is where the Rockies play!"
+//     },
+//     "geometry": {
+//         "type": "Point",
+//         "coordinates": [-104.99404, 39.75621]
+//     }
+// };
 
-L.geoJSON(geojsonFeature, {
-    onEachFeature: onEachFeature
-}).addTo(map);
+// L.geoJSON(geojsonFeature, {
+//     onEachFeature: onEachFeature
+// }).addTo(map);
 
-var someFeatures = [{
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "show_on_map": true
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-}, {
-    "type": "Feature",
-    "properties": {
-        "name": "Busch Field",
-        "show_on_map": false
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.98404, 39.74621]
-    }
-}];
+// var someFeatures = [{
+//     "type": "Feature",
+//     "properties": {
+//         "name": "Coors Field",
+//         "show_on_map": true
+//     },
+//     "geometry": {
+//         "type": "Point",
+//         "coordinates": [-104.99404, 39.75621]
+//     }
+// }, {
+//     "type": "Feature",
+//     "properties": {
+//         "name": "Busch Field",
+//         "show_on_map": false
+//     },
+//     "geometry": {
+//         "type": "Point",
+//         "coordinates": [-104.98404, 39.74621]
+//     }
+// }];
 
-L.geoJSON(someFeatures, {
-    filter: function(feature, layer) {
-        return feature.properties.show_on_map;
-    }
-}).addTo(map);
+// L.geoJSON(someFeatures, {
+//     filter: function(feature, layer) {
+//         return feature.properties.show_on_map;
+//     }
+// }).addTo(map);
